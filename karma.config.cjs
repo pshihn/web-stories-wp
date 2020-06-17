e8ec425a556a40ec5d45439b004d80fd2a991364
Pascal Birchler <pascalb@google.com>
2020-06-17 22:13:03 +0200

    Karma: collect code coverage

--- karma.config.cjs
+++ karma.config.cjs
@@ -28,8 +28,11 @@ module.exports = function (config) {
       'karma-jasmine',
       'karma-sourcemap-loader',
       'karma-webpack',
+      'karma-coverage',
       require('./karma/karma-puppeteer-launcher/index.cjs'),
       require('./karma/karma-puppeteer-client/index.cjs'),
+      require('./karma/karma-puppeteer-launcher'),
+      require('./karma/karma-puppeteer-client'),
     ],
 
     // Frameworks to use.
@@ -55,6 +58,7 @@ module.exports = function (config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'assets/src/edit-story/**/karma/**/*.js': ['webpack', 'sourcemap'],
+      'assets/src/**/*.js': ['coverage'],
     },
 
     proxies: {
@@ -72,7 +76,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
@@ -117,6 +121,13 @@ module.exports = function (config) {
 
     // Allow not having any tests
     failOnEmptyTestSuite: false,
+
+    // Configure coverage output
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'build/karma-coverage',
+      includeAllSources: true,
+    },
   });
 };
 
